76a838b608a2a6a417284a84699bdd3dd1c41267
Zulker Nien <zulkerb9b@gmail.com>
2022-11-10 03:05:44 +0600

    tried

--- Components/util.ts
+++ Components/util.ts
@@ -4,5 +4,5 @@ import { proxy } from "valtio";
 export const damp = THREE.MathUtils.damp;
 export const state = proxy({
   clicked: null,
-  urls: [1, 20, 3, 4, 5, 6, 7, 8, 9, 10].map((u) => `./Artwork/${u}.jpg`),
+  urls: [1, 20, 3, 4, 5, 6].map((u) => `/Artwork/${u}.jpg`),
 });
